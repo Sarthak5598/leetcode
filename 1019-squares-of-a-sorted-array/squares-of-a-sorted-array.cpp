d25c23cfd12ec9f5e2939222310fdf7b0a35980b
Sarthak5598 <108613416+Sarthak5598@users.noreply.github.com>
2024-02-05 20:14:41 +0530

    Time: 88 ms (5.09%) | Memory: 29.3 MB (17.30%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -5,15 +5,17 @@ public:
         vector<int>result;
         while(head<=tail){
             if(nums[head]*nums[head]>nums[tail]*nums[tail]){
-                result.push_back(nums[head]*nums[head]);
+                //result.push_back(nums[head]*nums[head]);
+                result.insert(result.begin(),nums[head]*nums[head]);
                 head++;
             }
             else{
-                result.push_back(nums[tail]*nums[tail]);
+                //result.push_back(nums[tail]*nums[tail]);
+                result.insert(result.begin(),nums[tail]*nums[tail]);
                 tail--;
             }
         }
-        reverse(result.begin(),result.end());
+        //reverse(result.begin(),result.end());
         return result;
     }
 };
